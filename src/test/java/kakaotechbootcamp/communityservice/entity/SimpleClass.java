694f784908522cb21b8a38ce4e5d30cfc4e7376b
dokkang01 <kimd02.dev@gmail.com>
2025-11-27 13:46:39 +0900

    edit: change test code logic to a simple lightweight code

---
+++ src/test/java/kakaotechbootcamp/communityservice/entity/SimpleClass.java
@@ -0,0 +1,13 @@
+package kakaotechbootcamp.communityservice.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleClass {
+    @Test
+    void simpleCheck(){
+        String result = "Hello".toUpperCase();
+        assertThat(result).isEqualTo("HELLO");
+    }
+}
