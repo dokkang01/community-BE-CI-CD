3fb4cd2069d183a9191a91df8c05b17195f2f085
dokkang01 <kimd02.dev@gmail.com>
2025-11-28 16:46:04 +0900

    add: image upload feature

--- src/main/java/kakaotechbootcamp/communityservice/config/S3Config.java
+++ src/main/java/kakaotechbootcamp/communityservice/config/S3Config.java
@@ -3,6 +3,8 @@ package kakaotechbootcamp.communityservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
--- src/main/java/kakaotechbootcamp/communityservice/service/ImageService.java
+++ src/main/java/kakaotechbootcamp/communityservice/service/ImageService.java
@@ -1,6 +1,6 @@
 package kakaotechbootcamp.communityservice.service;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value; //
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
